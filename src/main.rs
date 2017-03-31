baef10e0d85b4d246893cb32cefc92594f5df116
Daniel Slocombe <ds4314@ic.ac.uk>
2017-03-31 01:14:13 +0100

    Cleanup for github

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,11 @@ use piston::input::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{Colored, Textured, GlGraphics, OpenGL};
 use rand::Rng;
+use std::time::SystemTime;
 
 pub struct App {
     time : f64,
     gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64, // Rotation for the square.
 }
 
 struct Point(f64, f64, f64);
@@ -35,7 +35,7 @@ const PI: f64 = 3.141;
 
 fn plot_points(time : f64, gl : &mut GlGraphics, args : &RenderArgs) {
     let mut rng = rand::thread_rng();
-    let offset = rng.gen::<f64>();
+    //let offset = rng.gen::<f64>();
     let mut points = generate_init_points(0.0, 1.0, 20);
     let ticks = 30;
     let params = VFParams {
@@ -44,13 +44,13 @@ fn plot_points(time : f64, gl : &mut GlGraphics, args : &RenderArgs) {
         a3 : 0,
         p : 0.0,
         q : 0.1,
-        r : 0.1 + time/1000.0,
+        r : 0.1 + (time/1000.0).sin(),
         s : 0.5,
         v : 0.1,
         w : 0.2,
         u : 0.2,
     };
-    for i in 1..ticks {
+    for _ in 1..ticks {
         let mut points_new = Vec::new();
         for p in points {
             let p_new = mutate_point(1.0, &p, &params);
@@ -138,39 +138,14 @@ impl App {
             clear(BLACK, gl);
         });
         plot_points(self.time, &mut self.gl, args);
-        /*
-        use graphics::*;
-
-        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
-        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let (x, y) = ((args.width / 2) as f64, (args.height / 2) as f64);
-
-        self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            clear(BLACK, gl);
-
-            let transform = c.transform
-                .trans(x, y)
-                .rot_rad(rotation)
-                .trans(-25.0, -25.0);
-
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
-        });
-        */
     }
 
     fn update(&mut self, args: &UpdateArgs) {
-        // Rotate 2 radians per second.
-        self.rotation += 8.0 * args.dt;
+        self.time += 1.0;
     }
 }
 
 fn main() {
-    // Change this to OpenGL::V2_1 if not working.
     let opengl = OpenGL::V4_3;
 
     // Create an Glutin window.
@@ -180,28 +155,31 @@ fn main() {
         .build()
         .unwrap();
 
-    // Create a new game and run it.
-
     let c = Colored::new(opengl.to_glsl());
     let t = Textured::new(opengl.to_glsl());
     let glgraphics = GlGraphics::from_colored_textured(c, t);
     let mut app = App {
         time : 0.0,
         gl: glgraphics,
-        rotation: 0.0,
     };
 
+    let mut prev_time = SystemTime::now();
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
+
+            //  Print framerate
+            let dt = prev_time.elapsed().unwrap();
+            prev_time = SystemTime::now();
+            print!("fps {:.3}\r",
+                   1000.0 * 1000.0 * 1000.0 / ((dt.subsec_nanos())) as f64);
+
             app.render(&r);
         }
 
         if let Some(u) = e.update_args() {
             app.update(&u);
-            println!("{:.2}", 1.0/u.dt);
-            app.time += 1.0;
         }
     }
 }
