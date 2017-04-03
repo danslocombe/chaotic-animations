ee8527cc8631554edf4a4f206725f58410a419d2
Daniel Slocombe <ds4314@ic.ac.uk>
2017-04-03 08:34:47 +0100

    Modify update function, add customizability

--- src/main.rs
+++ src/main.rs
@@ -20,15 +20,25 @@ const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
 pub struct App {
     time : f64,
+    time_max : f64,
+    time_min : f64,
+    time_forward : bool,
+    time_mult : f64,
     gl: GlGraphics,
     wavefront : Vec<Point>,
     wavefront_prev : Vec<Point>,
     params : VFParams,
+    point_offset : f64,
+    point_radius : f64,
+    point_count : usize,
+    draw_width : f64,
+    plot_style : PlotStyle
 }
 
 #[derive(Clone)]
 struct Point(f64, f64, f64);
 
+#[derive(Clone)]
 struct VFParams {
     a1: usize,
     a2: usize,
@@ -44,11 +54,29 @@ struct VFParams {
 
 const PI: f64 = 3.141;
 
-fn plot_points(gl : &mut GlGraphics, args : &RenderArgs, wavefront : &[Point], wavefront_prev : &[Point]){
+enum PlotStyle {
+    Point,
+    Line,
+    Radial,
+}
+
+fn plot_points(gl : &mut GlGraphics, args : &RenderArgs, plot_style : &PlotStyle, width : f64, wavefront : &[Point], wavefront_prev : &[Point]){
     //  Assume wavefront length = wavefront_prev length
     for (i, p) in wavefront.iter().enumerate() {
         let color = gen_color(i as f64 / wavefront.len() as f64);
-        line_points(gl, args, color, &p, &wavefront_prev[i]);
+        match *plot_style {
+            PlotStyle::Point => {
+                line_points(gl, args, color, width, &p, &wavefront_prev[i]);
+            }
+            PlotStyle::Line => {
+                if i > 0 {
+                    line_points(gl, args, color, width, &p, &wavefront[i-1]);
+                }
+            }
+            PlotStyle::Radial => {
+                line_points(gl, args, color, width, &p, &Point(0.0, 0.0, 0.0));
+            }
+        }
     }
 }
 
@@ -57,7 +85,7 @@ fn gen_color(i : f64) -> Color {
 }
 
 
-fn line_points(gl : &mut GlGraphics, args : &RenderArgs, color : Color, p1 : &Point, p2 : &Point) {
+fn line_points(gl : &mut GlGraphics, args : &RenderArgs, color : Color, width : f64, p1 : &Point, p2 : &Point) {
     use graphics::*;
 
     let (x_mid, y_mid) = ((args.width / 2) as f64, (args.height / 2) as f64);
@@ -75,7 +103,7 @@ fn line_points(gl : &mut GlGraphics, args : &RenderArgs, color : Color, p1 : &Po
     gl.draw(args.viewport(), |c, gl| {
         let transform = c.transform;
         let l = [x_start, y_start, x_end, y_end];
-        line(color, 1.0, l, transform, gl);
+        line(color, width, l, transform, gl);
     });
 }
 
@@ -124,20 +152,48 @@ fn vector_field(p: &Point, params: &VFParams) -> Point {
 }
 
 impl App {
+    fn new(glgraphics : GlGraphics, params : VFParams, plot_style : PlotStyle) -> Self {
+
+        let mut app = App {
+            time : 0.0,
+            time_max : 100.0,
+            time_min : 0.0,
+            time_forward : true,
+            time_mult : 40.0,
+            gl: glgraphics,
+            wavefront : Vec::new(),
+            wavefront_prev : Vec::new(),
+            params : params,
+            point_offset : 0.0,
+            point_radius : 1.0,
+            point_count : 100,
+            draw_width : 1.0,
+            plot_style : plot_style,
+        };
+
+        app.reset_wavefront();
+
+        app
+    }
+    fn reset_wavefront(&mut self){
+        let init_points = generate_init_points(self.point_offset, self.point_radius, self.point_count);
+        self.wavefront = init_points.clone();
+        self.wavefront_prev = init_points;
+    }
     fn render(&mut self, args: &RenderArgs) {
         use graphics::*;
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
-            //clear(BLACK, gl);
+            clear(BLACK, gl);
         });
-        plot_points(&mut self.gl, args, &self.wavefront, &self.wavefront_prev);
+        plot_points(&mut self.gl, args, &self.plot_style, self.draw_width, &self.wavefront, &self.wavefront_prev);
         self.wavefront_prev = self.wavefront.clone();
     }
 
     fn update(&mut self, args: &UpdateArgs) {
         self.time += 1.0;
-        
-        self.wavefront = self.wavefront.iter().map(|p| mutate_point(40.0 * args.dt, p, &self.params)).collect();
+        let dt = 0.3 * (self.time / self.time_max).sin();
+        self.wavefront = self.wavefront.iter().map(|p| mutate_point(dt, p, &self.params)).collect();
     }
 }
 
@@ -155,46 +211,45 @@ fn main() {
     let t = Textured::new(opengl.to_glsl());
     let glgraphics = GlGraphics::from_colored_textured(c, t);
 
-    let initial_points = generate_init_points(0.0, 1.0, 50);
-
     let params = VFParams {
-        a1 : 1,
-        a2 : 2,
-        a3 : 0,
+        a1 : 2,
+        a2 : 0,
+        a3 : 1,
         p : 0.0,
         q : 0.1,
         r : 0.1,
-        s : 0.5,
+        s : 0.0,
         v : 0.1,
-        w : 0.2,
-        u : 0.2,
+        w : 0.0,
+        u : 0.0,
     };
 
-    let mut app = App {
-        time : 0.0,
-        gl: glgraphics,
-        wavefront : initial_points.clone(),
-        wavefront_prev : initial_points,
-        params : params,
-    };
-
+    let mut app = App::new(glgraphics, params, PlotStyle::Point);
     let mut prev_time = SystemTime::now();
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
-        if let Some(r) = e.render_args() {
+        match e {
+            Input::Render(r) => {
 
-            //  Print framerate
-            let dt = prev_time.elapsed().unwrap();
-            prev_time = SystemTime::now();
-            print!("fps {:.3}\r",
-                   1000.0 * 1000.0 * 1000.0 / ((dt.subsec_nanos())) as f64);
+                //  Print framerate
+                let dt = prev_time.elapsed().unwrap();
+                prev_time = SystemTime::now();
+                print!("fps {:.3}\r",
+                       1000.0 * 1000.0 * 1000.0 / ((dt.subsec_nanos())) as f64);
 
-            app.render(&r);
-        }
+                app.render(&r);
+            }
 
-        if let Some(u) = e.update_args() {
-            app.update(&u);
+            Input::Update(u) => {
+                app.update(&u);
+            }
+            Input::Press(i) => {
+                if i == Button::Keyboard(Key::Space) {
+                    app.reset_wavefront();
+                }
+            }
+            _ => {}
         }
     }
 }
